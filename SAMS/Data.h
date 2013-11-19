015bd720fe17fb92a022df82a55b5b539c2092c1
QingDengKuZhu <2583746755@qq.com>
2013-11-19 13:36:08 +0800

    设计数据结构
    设计STUDENT结构

--- SAMS/Data.h
+++ SAMS/Data.h
@@ -5,5 +5,24 @@
 #ifndef DATA_H
 #define DATA_H
 
+/*
+**学生数据结构
+*/
+typedef struct student 
+{
+	char ID[10];				/**学号**/
+	char name[15];				/*姓名*/
+
+	unsigned int cgrade;		/*C语言成绩*/
+	unsigned int egrade;		/*英语成绩*/
+	unsigned int mgrade;		/*数学成绩*/
+	
+	unsigned int total;			/*总分*/
+	double ave;					/*平均成绩*/
+	
+	unsigned int mingci;		/*名次*/
+
+}STUDENT;
+
 
 #endif
