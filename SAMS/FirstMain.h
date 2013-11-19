9b39ef23b7c044f09956b2feb8538d4d40b617e0
QingDengKuZhu <2583746755@qq.com>
2013-11-19 16:08:34 +0800

    添加相关宏定义

--- SAMS/FirstMain.h
+++ SAMS/FirstMain.h
@@ -5,6 +5,11 @@
 #ifndef FIRSTMAIN_H
 #define FIRSTMAIN_H
 
+#define FILE_PATH	"DATABASE"	/*指定数据库文件名,若未指定路径,则在当前工作目录*/
+#define HEADER1		"      ----------------------------STUDENT----------------------------------  \n"	
+#define FORMAT		"        |    %-10s |%-15s|%4d|%4d|%4d| %4d   | %.2f |%4d |\n"
+#define DATA		p->data.ID,p->data.name,p->data.egrade,p->data.mgrade,p->data.cgrade,p->data.total,p->data.ave,p->data.mingci /*不能用空格分隔*/
+#define END			"       --------------------------------------------------------------------- \n"
 
 
 
