c77009351c5353feb131c0675807de79bd7d0931
QingDengKuZhu <2583746755@qq.com>
2013-11-19 13:31:42 +0800

    规划文件
    确定各自文件内容

---
+++ SAMS/main.c
@@ -0,0 +1,14 @@
+/*************************************
+**系统主函数
+**************************************/
+
+#include "Data.h"
+#include "DataFucntion.h"
+#include "FirstMain.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	return EXIT_SUCCESS;
+}
